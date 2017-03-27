f6699a68ca30258935432666695c796e1b956076
tom.shen <tom.shen@oracle.com>
2017-03-27 15:04:02 +0800

    Add boost and codelite build

---
+++ docker/buildwithdocker/build-boost.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export VERSION=1.63.0
+
+yum install -y -q zlib-devel bzip2-devel p7zip which libicu-devel python-devel
+
+./b2 -j 16 toolset=gcc cxxflags=-std=c++11 stage
+
--- docker/buildwithdocker/config-yum-local.sh
+++ docker/buildwithdocker/config-yum-local.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-if [ -n $http_proxy ]; then sed -i "/^installonly_limit/i proxy=$http_proxy" /etc/yum.conf ; fi && \
-echo "10.113.69.25   localyummirror" >> /etc/hosts && \
-rm -f /etc/yum.repos.d/* && \
-(cd /etc/yum.repos.d/; curl --noproxy "*" -s -k -O https://localyummirror/mirror/repos/centos/centos-local.repo) && \
-sed -i "s/enabled=0/enabled=1/g" /etc/yum.repos.d/*
-yum install -y -q autoconf automake gcc gcc-c++ make file deltarpm xz
+sed -i "/^installonly_limit/i proxy=http://10.113.69.101:3128" /etc/yum.conf
+yum install -y -q yum-plugin-fastestmirror epel-release
+echo "include_only=ftp.iij.ad.jp" >> /etc/yum/pluginconf.d/fastestmirror.conf
+yum install -y -q autoconf automake gcc gcc-c++ make file which
