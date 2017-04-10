9106b71d2f31967db790629bd98aff7041c3bb68
tom.shen <tom.shen@oracle.com>
2017-04-10 15:18:24 +0800

    Add protobuf build

--- docker/buildwithdocker/build-git.sh
+++ docker/buildwithdocker/build-git.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export VERSION=2.12.0
+export VERSION=2.12.2
 yum install -y -q curl-devel expat-devel gettext-devel openssl-devel perl-devel zlib-devel asciidoc xmlto docbook2X
 ln -s /usr/bin/db2x_docbook2texi /usr/bin/docbook2x-texi
 curl -L https://www.kernel.org/pub/software/scm/git/git-${VERSION}.tar.xz | tar -xJ -C ~
---
+++ docker/buildwithdocker/build-protobuf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export VERSION=3.2.0
+yum install -y -q gcc-c++ unzip autoconf libtool
+curl -L https://github.com/google/protobuf/archive/v{version}.tar.gz | tar -xJ -C ~
+cd ~/protobuf-${VERSION}
+./autogen.sh
+./configure -q --build=x86_64-pc-linux --host=x86_64-pc-linux --target=x86_64-pc-linux
+make -j $(nproc)
+make install-strip
