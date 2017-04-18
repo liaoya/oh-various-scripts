5eaebd85847b5163f7e43ff0ef01eeb2a834056b
tom.shen <tom.shen@oracle.com>
2017-04-18 17:06:07 +0800

    Small fix

--- docker/buildwithdocker/build-grpc.sh
+++ docker/buildwithdocker/build-grpc.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 VERSION=1.2.3
-yum install -y -q zlib-devel openssl-devel
+yum install -y -q zlib-devel openssl-devel gnutls-devel
 
-curl -L https://github.com/grpc/grpc/archive/v${VERSION}.tar.gz | tar -xJ -C ~
+curl -L https://github.com/grpc/grpc/archive/v${VERSION}.tar.gz | tar -xz -C ~
 cd grpc-${VERSION}/third_party
 rm -fr protobuf
 PROTOBUF_VERSION=3.2.0
-curl -L https://github.com/google/protobuf/archive/v{PROTOBUF_VERSION}.tar.gz | tar -xJ -C ~
+curl -L https://github.com/google/protobuf/archive/v{PROTOBUF_VERSION}.tar.gz | tar -xz -C ~
 mv protobuf-${PROTOBUF_VERSION} protobuf
 cd ..
 make -j $(nproc)
 make install
-for item in $(ls -1 /usr/local/bin/*); do strip $item; done
+for item in $(ls -1 /usr/local/bin/*); do [ -x $item ] && strip $item; done
--- docker/buildwithdocker/build-vim.sh
+++ docker/buildwithdocker/build-vim.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-VERSION=8.0.0386
+yum install -y -q ncurses-devel \
+  ruby ruby-devel lua lua-devel luajit \
+  luajit-devel ctags git python python-devel \
+  python3 python3-devel tcl-devel \
+  perl perl-devel perl-ExtUtils-ParseXS \
+  perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
+  perl-ExtUtils-Embed \
+  gtk2-devel gtk3-devel \
+  libacl-devel cscope
+
+VERSION=8.0.0566
 curl -L https://github.com/vim/vim/archive/v${VERSION}.tar.gz | tar -xz -C ~
-yum install -y -q ncurses-devel
+
 cd ~/vim-${VERSION}
 ./configure -q --with-features=tiny --build=x86_64-pc-linux --host=x86_64-pc-linux --target=x86_64-pc-linux
 make -j $(nproc) install
-tar -Jcf ~/vim-minimal-${VERSION}.txz -C /usr/local .
-rm -fr /usr/local/*
-make clean
+
+# ./configure --build=x86_64-pc-linux --host=x86_64-pc-linux --target=x86_64-pc-linux --with-features=huge --enable-pythoninterp --enable-cscope
+# ./configure --build=x86_64-pc-linux --host=x86_64-pc-linux --target=x86_64-pc-linux --with-features=huge --enable-multibyte --enable-rubyinterp --enable-pythoninterp --enable-python3interp --enable-perlinterp --enable-luainterp --enable-cscope --enable-gui=gtk3
