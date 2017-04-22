87ba29d3409e85125a9eda7b21de43d490075b82
tom.shen <tom.shen@oracle.com>
2017-04-22 08:32:13 +0800

    Build Python 3.6

--- docker/buildwithdocker/build-python.sh
+++ docker/buildwithdocker/build-python.sh
@@ -6,9 +6,11 @@ VERSION=3.6.1
 curl -L https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz | tar -xz -C ~/
 
 cd Python-${VERSION}
-./configure --enable-optimizations --enable-shared
+./configure -q --enable-optimizations --enable-shared --build=x86_64-pc-linux --host=x86_64-pc-linux --target=x86_64-pc-linux
 make
 make altinstall
-for item in $(ls -1 /usr/local/bin/python3*); do [ -x $item ] && strip $item; done
+for item in $(ls -1 /usr/local/bin/python3*); do file $item | grep -q -s "not stripped" && strip $item; done 
+for item in $(ls -1 /usr/local/lib/libpython3*); do file $item | grep -q -s "not stripped" && strip $item; done 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 /usr/local/bin/pip3.6 install -U virtualenv
 mv /usr/local/bin/virtualenv /usr/local/bin/virtualenv-3.6
