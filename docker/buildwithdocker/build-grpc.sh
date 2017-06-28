e4c9161f15df26ef093a928af16eb81b377994fa
tom.shen <tom.shen@oracle.com>
2017-06-28 14:36:39 +0800

    Update GRPC to 1.4.0

--- docker/buildwithdocker/build-grpc.sh
+++ docker/buildwithdocker/build-grpc.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# Need Improve
+# c-ares-devel in CentOS 7 is too old
+# Only 1.12.0 can be work with GRPC 1.4.0
 
-yum install -y -q zlib-devel openssl-devel gnutls-devel c-ares-devel
+yum install -y -q zlib-devel openssl-devel gnutls-devel
 
 PROTOBUF_VERSION=3.3.0
 curl -L https://github.com/google/protobuf/archive/v${PROTOBUF_VERSION}.tar.gz | tar -xz -C ~
@@ -11,7 +12,7 @@ cd ~/protobuf-${PROTOBUF_VERSION}
 ./configure -q
 make -j $(nproc) install-strip
 
-GRPC_VERSION=1.3.4
+GRPC_VERSION=1.4.0
 curl -L https://github.com/grpc/grpc/archive/v${GRPC_VERSION}.tar.gz | tar -xz -C ~
 cd ~/grpc-${GRPC_VERSION}/third_party/cares
 CARES_VERSION=1.12.0
@@ -20,11 +21,11 @@ curl -L https://c-ares.haxx.se/download/c-ares-${CARES_VERSION}.tar.gz | tar -xz
 cd ~/grpc-${GRPC_VERSION}/
 make -j $(nproc)
 make -j $(nproc) strip install
-for item in $(ls -1 /usr/local/bin/grpc*plugin); do file $item | grep -q -s "not stripped" && strip $item; done
+for item in $(ls -1 /usr/local/bin/grpc*plugin); do file $item | grep -q -s "not stripped" && strip -S $item; done
 
-JAVA_VERSION=1.3.0
-curl -L https://github.com/grpc/grpc-java/archive/v${JAVA_VERSION}.tar.gz | tar -xz -C ~
-cd ~/grpc-java-${JAVA_VERSION}/compiler/src/java_plugin/cpp
+GRPC_JAVA_VERSION=1.4.0
+curl -L https://github.com/grpc/grpc-java/archive/v${GRPC_JAVA_VERSION}.tar.gz | tar -xz -C ~
+cd ~/grpc-java-${GRPC_JAVA_VERSION}/compiler/src/java_plugin/cpp
 g++ -std=c++11 -I/usr/local/include -pthread -L/usr/local/lib -lprotoc -lprotobuf -lpthread -ldl -s -o /usr/local/bin/grpc_java_plugin *.cpp
 # git submodule update --recursive --remotecd
 # make run_dep_checks
